d49dbbff7f83bb06f9160e96bd73c142d8f7c756
Padmank Tiwari <padmank.tiwari@pilottravelcenters.com>
2024-08-08 13:16:16 +0530

    Initial commit

---
+++ app/src/main/java/com/example/designsystemdemo/ui/theme/PrimaryButton.figma.kt
@@ -0,0 +1,16 @@
+import androidx.compose.runtime.Composable
+import com.figma.code.connect.FigmaConnect
+import com.figma.code.connect.FigmaProperty
+import com.figma.code.connect.FigmaType
+import kotlin.String
+
+@FigmaConnect("https://www.figma.com/design/xJgJWGR46sWblerwaOHla8/FigmaConnect?node-id=4%3A406")
+public class PrimaryButtonDoc {
+    @FigmaProperty(FigmaType.Text, "Text Content")
+    public val textContent: String = "Sign Up"
+
+    @Composable
+    public fun ComponentExample() {
+        /* Add your component code here. */
+    }
+}
